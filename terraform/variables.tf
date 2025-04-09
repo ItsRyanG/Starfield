5f3362524c00788aaf9b7ac4f61ea1b7dd2ac0c3
ItsRyanG <ryan@agffa.ca>
2025-04-09 00:14:53 -0400

    cleanup: remove unused aws_access_key and aws_secret_key variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,3 @@
-variable "aws_access_key" {
-  description = "AWS access key for authentication"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS secret key for authentication"
-  type        = string
-  sensitive   = true
-}
-
 variable "aws_region" {
   description = "AWS region for deployment"
   type        = string
